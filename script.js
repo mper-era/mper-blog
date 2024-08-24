33f002ea11750c7690a07a9976a14a8c9f75baf2
Cat-Code-Pixel <tomatoesintomatoes@gmail.com>
2024-08-24 15:43:56 -0500

    File reading update 1

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const fs = require("fs");
- 
-fs.readFile("Posts/Content/test.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    
-    data = data.toString();
+
+const content = [];
+
+fs.readdir("Posts/Content/", (err, files) => {
+    if (err) {console.log("Error reading directory"); process.exit(1);}
+
+    files.forEach((file, index) => {
+        let ext = file.split('.').pop();
+        if (ext == "txt") {
+            fs.readFile(`Posts/Content/${file}`, "utf8", (err, data) => {
+                if (err) {console.log(`Error reading file ${file} numbered ${index}`); process.exit(1);}
+                
+                data = data.toString();
+        
+                content.push(data);
+            });
+        }
+    })
+
+    document.getElementById("test").innerHTML = content[2];
+    alert(content[2]);
 });
